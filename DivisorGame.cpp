7a1653d3be3738cb469612cc59aef795036ea789
Luis <owenmicro3000@gmail.com>
2024-06-16 03:02:38 -0400

    Divisor Game

--- DivisorGame.cpp
+++ DivisorGame.cpp
@@ -1,3 +1,44 @@
+/*
+
+1025. Divisor Game
+
+Alice and Bob take turns playing a game, with Alice starting first.
+
+Initially, there is a number n on the chalkboard. 
+On each player's turn, that player makes a move consisting of:
+
+Choosing any x with 0 < x < n and n % x == 0.
+Replacing the number n on the chalkboard with n - x.
+Also, if a player cannot make a move, they lose the game.
+
+Return true if and only if Alice wins the game, assuming both players play optimally.
+
+
+Constraints:
+1 <= n <= 1000
+
+
+Approach:
+
+We use a dynamic programming approach.
+The sub-problem is
+
+f(n):= true/false if ALice wins/loses the game when it starts with integer n
+
+f(1)= false
+f(n) = true   if there exists a factor of n, i, where 0<i<n such that f(i)=false
+       false  otherwise 
+
+We start with the base case and continue calculating f of the next integer until we reach n.
+
+
+time complexity: O(n^2)
+
+*/
+
+
+
+
 #include<iostream>
 #include<vector>
 
