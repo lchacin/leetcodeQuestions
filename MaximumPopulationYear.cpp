2bebe0c6ec17b8cb74e7e5f3794d09800bb2e418
Luis <owenmicro3000@gmail.com>
2024-08-23 05:28:20 -0400

    1854. Maximum Population Year

--- MaximumPopulationYear.cpp
+++ MaximumPopulationYear.cpp
@@ -1,3 +1,25 @@
+/*
+
+1854. Maximum Population Year
+
+You are given a 2D integer array logs where each logs[i] = [birthi, deathi] 
+indicates the birth and death years of the ith person.
+
+The population of some year x is the number of people alive during that year. 
+The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. 
+Note that the person is not counted in the year that they die.
+
+Return the earliest year with the maximum population.
+
+
+Constraints:
+
+1 <= logs.length <= 100
+1950 <= birthi < deathi <= 2050
+
+*/
+
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
